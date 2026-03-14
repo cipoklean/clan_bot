8fa020ffa89ce7b7bd13c977a9cda69f44004f5b
cipoklean <aghaziedavid@gmail.com>
2026-03-14 22:50:41 +0100

    add remove,clear,nextevent and help commands

--- clan_bot.py
+++ clan_bot.py
@@ -48,6 +48,54 @@ async def events(ctx):
             message += f"{i}. {event['event']} - {event['date']} at {event['time']}\n"
         await ctx.send(message)
 
+@bot.command()
+async def removeevent(ctx, number:int):
+    if number < 1 or number > len(clan_events):
+        await ctx.send("Invaild event number! use !events to see the list.")
+    else:
+        removed = clan_events.pop(number - 1)
+        await ctx.send(f"removed: **{removed['event']}** - {removed['date']} at {removed['time']}")
+
+@bot.command()
+async def clearevents(ctx):
+    clan_events.clear()
+    await ctx.send("All events have been cleared!")
+
+@bot.command()
+async def nextevent(ctx):
+    if len(clan_events) == 0:
+        await ctx.send(" No upcoming events! Use !addevent to add one.")
+    else:
+        event = clan_events[0]
+        await ctx.send(f" **Next Event:**\n📌 {event['event']}\n📅 {event['date']}\n⏰ {event['time']}")
+@bot.command(name='help')
+async def help_command(ctx):
+    await ctx.send("""
+⚔️ **LastZ Clan Bot Commands:**
+
+**!addevent [name] [date] [time]**
+   Add a new clan event
+   Example: !addevent Raid Saturday 8PM
+
+ **!events**
+   Show all upcoming events
+
+ **!nextevent**
+   Show the next upcoming event
+                   
+ **!nextevent**
+   Show the next upcoming event
+
+ **!removeevent [number]**
+   Remove an event by its number
+   Example: !removeevent 1
+
+ **!clearevents**
+   Clear all events
+
+ **!help**
+   Show this help message""")
+                   
 @tasks.loop(hours=24)
 async def daily_reminder():
     channel = discord.utils.get(bot.get_all_channels(), name='general')
